1722bc34c3246bcb18960f59b20185fca577be9b
AlinaBarkovskaa <a.barkovskaa@icloud.com>
2022-11-24 13:42:53 +0400

    chenged task

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,11 +1,23 @@
-﻿Console.Clear();
+﻿// Console.Clear();
+// Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// string numberText =  Convert.ToString(number);
+// if (numberText.Length > 2)
+// {
+//    Console.WriteLine("Третья цифра этого числа = "+ numberText[2]);
+// }else 
+// {
+//   Console.WriteLine("Tретьей цифры нет");
+// }
+
+Console.Clear();
 Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 string numberText =  Convert.ToString(number);
-if (numberText.Length > 2)
-{
-   Console.WriteLine("Третья цифра этого числа = "+ numberText[2]);
-}else 
-{
-  Console.WriteLine("Tретьей цифры нет");
-}
+if( number < int.MaxValue && number > 99)
+   {
+   Console.WriteLine("Третья цифра этого числа = " + numberText[2]);
+   } else 
+   {
+     Console.WriteLine("Tретьей цифры нет");
+   }
